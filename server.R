e22d685976ebcd82b875587a01e8da2eeb2da362
jingg46 <jingli940731@gmail.com>
2017-12-16 18:11:11 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -1,55 +1,148 @@
 library(shiny)
 library(shinydashboard)
+
 # Define server logic required to draw a plot
+
 shinyServer(function(input, output){
   
   #setwd("/Users/yeyazheng/Desktop/327")
   library(leaflet)
   library(DT)
   library(maps)
+  library(plyr)
   library(dplyr)
   library(ggplot2)
   library(gmapsdistance)
   library(ggmap)
   library(ggalt)
+  library(kimisc)
+  library(lubridate)
+  library(udunits2)
   
-  geo.office = read.csv("~/Desktop/Data Science Project/mva_data.csv")
+  geo.office = read.csv("./mva data.csv")
   des<-paste(geo.office$lat , geo.office$lon,sep = "+")
   
   ########################
-  ## Find the nearest office
+  ## Take user input
   ########################
   
+  source("Distance.Time.R")
+  source("prediction_model.R")
+  
+  output$slider1 <- renderUI({
+    slider2.value <- format(as.Date(input$id.date),"%A")
+    if (slider2.value=="Sunday"){
+      sliderInput("id.time","MVA is close on Sunday", 
+                  min = as.POSIXct("08:30:00", format = "%H:%M:%S"), 
+                  max = as.POSIXct("08:30:00", format = "%H:%M:%S"),
+                  value=as.POSIXct("08:30:00", format = "%H:%M:%S"),
+                  step=300,timeFormat = "%H:%M:%S", ticks = F, animate = T)
+    } else if(slider2.value=="Saturday"){
+      sliderInput("id.time","Select Time",
+                  min = as.POSIXct("08:30:00", format = "%H:%M:%S"),
+                  max=as.POSIXct("12:00:00", format = "%H:%M:%S"),
+                  value=as.POSIXct("09:00:00", format = "%H:%M:%S"),
+                  step=300,timeFormat = "%H:%M:%S", ticks = F, animate = T)
+    } else{sliderInput("id.time","Select Time", 
+                       min = as.POSIXct("08:30:00", format = "%H:%M:%S"), 
+                       max = as.POSIXct("16:30:00", format = "%H:%M:%S"),
+                       value=as.POSIXct("09:00:00", format = "%H:%M:%S"),
+                       step=300,timeFormat = "%H:%M:%S", ticks = F, animate = T)
+    }
+    
+  })
+  
+  ## 1. obtain user's location in plain english
   user.address = reactive({
     paste(paste(input$id.street ,input$id.city,"MD",sep=", "),input$id.zipcode)
   })
   
-  user.address.cord = reactive({
-    tryCatch({geocode(user.address(), source = "google")},
-             error=function(e){cat("The input address is not valid,try another one!\n")})#get the user's coordinates
-  })
   
-  gmapresult = reactive({
-    if(length(user.address.cord())>0){
-      ori = paste(user.address.cord()$lat,user.address.cord()$lon,sep="+")
-      mvadata = read.csv("https://raw.githubusercontent.com/adv-datasci/mva/master/mvadata.csv", sep = ",") %>%
-        mutate(day = as.character(day),
-               service = as.character(service),
-               office = as.character(office))
-      disinfor<-gmapsdistance(origin = ori, destination = des %>% as.vector(),
-                              mode = input$id.travel.method, 
-                              shape = "long")#compute the distance from origin to all the office locations
-      result = cbind(geo.office$Name, geo.office$Address,
-                     disinfor$Status, disinfor$Time$Time, disinfor$Distance$Distance) %>% 
-        setNames(c("Office","Address","or","de","status","Time (minutes)","Distance (miles)")) %>%
-        mutate(`Time (minutes)` = round(`Time (minutes)`/60, 0), # convert unit from sec to minute
-               `Distance (miles)` = round(`Distance (miles)` * 0.000621371,1)) # convert unit from m to mile
-      result
+  ## 2. obtain longtidue and latitude of user's location
+  user.address.cord = reactive({
+    x = try({ # try google geocode first
+      geocode(user.address(), source = "google")
+    }, silent = TRUE)
+    if(inherits(x, "try-error")){ 
+      x = try({ # try dsk geocode if google geocode fails
+        geocode(user.address(), source = "dsk")
+      }, silent = TRUE)
+    } 
+    if(inherits(x, "try-error")){ # if dsk geocode fails, return NULL
+      cat("coordiates not found")
+      return(NULL)
+    } else {
+      return(x)
     }
   })
   
-  closest_office = reactive({
-    geo.office[which.min(gmapresult()$`Distance (miles)`),]
+  
+  ## 3. alculate travel time and distance from the user location to MVA offices
+  output.data = reactive({
+    # we need to have valid longitde and latitude to proceed, otherwise we output the following error message
+    validate(
+      need(try(sum(is.na(user.address.cord())) == 0), "Retry in 5 seconds - Coordinates not found!")
+    )
+    validate(
+      need(try(format(as.Date(input$id.date),"%A")!="Sunday"), "MVA office is closed on Sunday!")
+    )
+    # we need to have future departure date, otherwise we output the following error message
+    validate(
+      need(try(as.Date(input$id.date) > Sys.Date()), "The departure date has to be some time in the future!")
+    )
+    
+    ori <- gsub(" ", "+", user.address())
+    disinfor <- try({gmapsdistance(origin = ori, destination = des %>% as.vector(),
+                                   mode = tolower(input$id.travel.method), 
+                                   dep_date = as.character((as.Date(input$id.date)+7)), 
+                                   dep_time = as.character(gsub("^.* ","", input$id.time)), 
+                                   shape = "long")}, silent = TRUE)
+    if(inherits(disinfor, "try-error")){
+      cat("fail to use the Google Maps Distance Matrix API to compute the distances and times between your location and MVA offices.")
+      disinfor = NULL 
+    } 
+    # we need to have valid travel distance and times, otherwise we output the following error message
+    validate(
+      need(try(is.null(disinfor) == FALSE), "Retry in 15 seconds - Travel distances and times are not computed!")
+    )
+    
+    output.data <- geo.office$Name %>% as.data.frame()
+    colnames(output.data)[1] <- "office"
+    output.data$est.time <- disinfor$Time[[3]]/60 %>% round(.,0)
+    output.data$dis <- udunits2::ud.convert(disinfor$Distance[[3]], "m", "km") %>% round(.,1)
+    arr.time <- seconds_to_period(disinfor$Time[[3]]+period_to_seconds(hms(as.character(gsub("^.* ","", input$id.time)))))
+    output.data$arr.time <- sprintf("%02i:%02i:%02i", hour(arr.time), minute(arr.time),second(arr.time))
+    output.data$day <- format(as.Date(input$id.date), "%A")
+    output.data
+  })
+  
+  
+  ## 4. incorporate travel time, wait time and choose the office that gives the earliest service start time
+  predict_result = reactive({
+    output.data = output.data()
+    #office <- levels(droplevels(output.data$office)) <- The order of office is different from output.data$office
+    office <- output.data$office %>% as.character()
+    service = gsub(pattern=" ",replacement="",input$id.visit.reason)
+    day = output.data$day[1]
+    arrival_time <- gsub("^.* ","", as.POSIXct(output.data$arr.time, format = "%H:%M:%S") - hours(5))
+    
+    predict_result <- choose_office(office, service, day, arrival_time)
+    
+    # we need to have office
+    validate(
+      need(try(sum(is.na(predict_result)) == 0), 
+           "You depart too late, so MVA will be closed by the time you receive the service.Or, the service is not available at any office by the time you arrive.")
+    )
+    
+    # the default time zone of shinyapps.io is UTC, which is 5 hours ahead of EST
+    predict_result <- as.data.frame(t(ldply(predict_result))) %>% 
+      mutate(V2 = as.numeric(as.character(V2)),
+             V3 = gsub("^.* ","", as.POSIXct(V3, format = "%H:%M:%S")),
+             V4 = round(as.numeric(as.character(V4)),1),
+             V5 = gsub("^.* ","", as.POSIXct(V5, format = "%H:%M:%S")))
+    #colnames(predict_result)=c("Office","Arrival Time","Waiting Time","Service Time")
+    rownames(predict_result) = NULL
+    predict_result
   })
   
   
@@ -57,27 +150,31 @@ shinyServer(function(input, output){
   ## Output results
   ########################
   
-  # output the nearest office
+  ## 5. output text
   output$office <- renderText({
-    
-    a=closest_office()
-    paste(a$Name, a$Address)
-    
+    predict_result <- predict_result()
+    paste("We recommend you to go to", predict_result$V1, "office to get", input$id.visit.reason,
+          "service. You are expected to arrive at the office at", predict_result$V3,
+          ", wait approximately", predict_result$V4, "minutes, and get the service at", predict_result$V5)
   })
   
-  # output map
+  ## 6. output map
   output$id.distPlot1 <- renderLeaflet({
     
-    a = closest_office()
+    # define variables 
+    a <- predict_result()
+    a = filter(geo.office, Name == as.character(a$V1))
     b = user.address()
     c = user.address.cord()
     
+    # generate map
     maryland = map("county","maryland",fill=TRUE, plot=FALSE)
-    leaflet(data = maryland) %>% 
-      addTiles() %>%
-      addPolygons(fillColor = topo.colors(10, alpha = 0.1), stroke = TRUE, color = "blue", weight = 2) %>%
+    leaflet(data = maryland) %>%
+      #addTiles() %>%
+      addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+      addPolygons(fillColor = topo.colors(10, alpha = 0.2), stroke = TRUE, color = "blue", weight = 2) %>%
       # set default view to Baltimore area
-      # setView(lng = -77.03687 , lat = 38.90719, zoom = 8) %>% 
+      # setView(lng = -77.03687 , lat = 38.90719, zoom = 8) %>%
       fitBounds(lng1 = a$lon, lat1 = a$lat, lng2 = c$lon, lat2 = c$lat) %>%
       addAwesomeMarkers(lng = a$lon, lat = a$lat, label = paste(a$Name, "office:", a$Address),
                         icon = makeAwesomeIcon(icon = "flag", markerColor = "black", iconColor = "white"),
@@ -87,51 +184,71 @@ shinyServer(function(input, output){
                         labelOptions = labelOptions(noHide = T))
   })
   
-  # output table
+  
+  ## 7. output table
   output$gmapresultTable <- DT::renderDataTable({
-    a = gmapresult() %>%
-      select(Office, Address, `Time (minutes)`, `Distance (miles)`)
+    
+    a = merge(output.data(), geo.office, by.x = "office", by.y = "Name")
+    a = a %>%
+      select(office, Address, est.time, dis, arr.time) %>%
+      mutate(est.time = round(est.time, 2),
+             dis = round(dis, 2),
+             arr.time = gsub("^.* ","", as.POSIXct(arr.time, format = "%H:%M:%S") - hours(5))) %>%arrange(arr.time)%>%
+      setNames(c("Office","Address","Travel Time (min)","Distance (km)","Arrival Time"))
     DT::datatable(a)
   })
   
-  output$id.distPlot2 <- renderPlot({
-    
-    office_closest=closest_office()
-    
-    sub.mvadata = mvadata %>%
-      filter(office == as.character(office_closest$Name) & service == gsub(pattern = " ", replacement =  "", x = input$id.visit.reason) & day == input$id.day) %>%
-      group_by(index) %>%
-      summarise(mean.wait.people = mean(num_people),
-                mean.wait.time = mean(wait_time)) %>%
-      as.data.frame()
-    
-    plot(sub.mvadata$index, sub.mvadata$mean.wait.time,
-         col = 'darkgray', pch = 16,
-         xlab = "Time Index (5 min)", ylab = "Mean Waiting Time (minutes)")
-    
+  output$approvalBox <- renderValueBox({
+    valueBox(
+      "80%", "Approval", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
   })
   
   
-  #  output$id.distPlot1 <- renderPlot({
-  # 
-  #    # Get User's Coordinates --------------------------------
-  #    address<-paste(paste(input$id.street ,input$id.city,"MD",sep=", "),input$id.zipcode)
-  #    address_loc=tryCatch({geocode(address,source = "google")},
-  #                         error=function(e){cat("The input address is not valid,try another one!\n")})
-  #    a=closet_office()
-  #    
-  #   if(length(address_loc)>0){
-  #     places_loc <- data.frame(lon=a$lon,lat=a$lat)  # get longitudes and latitudes
-  #     places_loc <- rbind(address_loc,places_loc)
-  #     qmap(as.character(input$id.city), zoom = 10,source="google", maptype="roadmap") +
-  #       geom_point(aes(x=lon, y=lat), data = places_loc, alpha = 0.7, size = 2, color = "tomato") + 
-  #       geom_encircle(aes(x=lon, y=lat), data = places_loc, size = 2, color = "blue") +
-  #       geom_text(aes(x=lon, y=lat+0.01), data = places_loc, label=c("Your Address","Closest MVA office"), cex=1.8, colour="red")
-  # 
-  #      }
-  #    
-  #  })
-  #  
+  ## 8. output wait time trend
+  output$id.distPlot2 <- renderPlot({
+    
+    # define variables
+    a <- predict_result()
+    b <- output.data()
+    office <- a$V1 %>% as.character()
+    time_index <- a$V2 %>% as.character() %>% as.numeric()
+    arrival_time <- a$V3
+    
+    # subset data based on office, service and day
+    load("mvadata.rda")
+    subset_idx <- which(mvadata$office == office & 
+                          mvadata$service == gsub(pattern = " ", replacement = "", x = input$id.visit.reason) & 
+                          mvadata$day == b$day[1])
+    subset_data <- mvadata[subset_idx,]
+    
+    model.lo <- loess(wait_time ~ index, subset_data,span=0.2)
+    max_idx <- max(subset_data$index)
+    min_idx <- min(subset_data$index)
+    prediction_curve <- predict(model.lo, data.frame(index = seq(min_idx, max_idx, 0.05)), se = TRUE)
+    # plot(seq(1,95,0.1),prediction$fit)
+    # points(subset_data$index,subset_data$wait_time,col="red")
+    
+    expect_wait <- predict(model.lo, data.frame(index = time_index), se = TRUE)$fit
+    if (expect_wait < 0){expect_wait <- 0}
+    subset_dataframe <- data.frame(timepoints = seq(min_idx,max_idx,0.05), predict_wait = prediction_curve$fit)
+    
+    ggplot(data = subset_dataframe, aes(x=timepoints, y=predict_wait)) + 
+      geom_line(size = 1) + theme_bw() +
+      labs(title = paste0("Waiting Time Trend for Service ", service," at ", office, " office")) +
+      scale_x_continuous(name = "Arrival Time") +
+      scale_y_continuous(name = "Expected waiting time (minutes)") + 
+      geom_segment(aes(x = time_index, y = 0, xend = time_index, yend = expect_wait), linetype="dotted", color = "blue", size=1) +
+      geom_segment(aes(x = 0, y = expect_wait, xend = time_index, yend = expect_wait), linetype="dotted", color = "blue", size=1) +
+      geom_segment(aes(x = time_index + 5, y = expect_wait + 2, xend = time_index - 0.3, yend = expect_wait + 0.2),
+                   arrow = arrow(length = unit(0.5, "cm")), color = "red") +
+      annotate("text", x = 3, y = expect_wait + 0.8, label= paste0(round(expect_wait, digits = 1), " minutes")) + 
+      annotate("text", x = time_index, y = -0.8, label= paste0(arrival_time))
+  })
   
+}
+)
   
-})
+
+ 
--- ui.R
+++ ui.R
@@ -1,42 +1,92 @@
 library(shinydashboard)
+library(leaflet)
 
 header <- dashboardHeader(
   title = "MVA Recommendation"
 )
 
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Instruction", tabName = "instruction", icon = icon("info-circle")),
+    menuItem("Recommended MVA Office", icon = icon("th"), tabName = "recommendation"),
+    menuItem("Wait Time Prediction", icon = icon("line-chart"), tabName = "prediction")
+  )
+)
+
 body <- dashboardBody(
-  fluidRow(
-    column(width = 8,
-      box(width = NULL, solidHeader = TRUE,
-        leafletOutput("id.distPlot1", height = 500)
-      ), 
-      box(width = NULL,
-          plotOutput("id.distPlot2", height = 500)
-          )
-    ), 
-    column(width = 3, 
-           box(width = NULL, status = "warning", title="User Information",
-           textInput(inputId="id.street", label="Street", value = ""),
-           textInput(inputId="id.city", label="City", value = ""),
-           textInput(inputId="id.zipcode", label="Zipcode", value = ""),
-           selectInput(inputId = "id.travel.method", label = "Select Mode of Travel", choices = c("driving","walking","bicycling","transit")),
-           selectInput(inputId = "id.visit.reason", label = "Select Service", choices = c("Driver License Renewal", "Insurance Compliance Division", "Learners Permit", "Other Drivers Services", "Other Vehicle Services", "Registration Renewal", "Tag Return","Title")),
-           selectInput(inputId = "id.day",
-                       label = "Select day",
-                       choices = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")),
-           sliderInput(inputId = "id.time",
-                       label = "Select Time",
-                       min = as.POSIXct("08:30:00", format = "%H:%M:%S"),
-                       max = as.POSIXct("16:30:00", format = "%H:%M:%S"),
-                       value = c(as.POSIXct("10:00:00", format = "%H:%M:%S"), as.POSIXct("14:00:00", format = "%H:%M:%S")),
-                                 step = 300, timeFormat = "%H:%M:%S", ticks = F, animate = T)))
+  tabItems(
+    tabItem(tabName = "instruction", 
+            h3("Description"),
+            "This Shiny app takes a users location and a specific time and date and lists which MVA office is best for them to visit to take care of their business as early as possible. This app incorporates travel distance and time between the users location and MVA offices and wait times for services offered at each of the MVA offices.",
+            h3("Methods"),
+            em("Data"),
+            br(),
+            br(),
+            "The MVA of Maryland posts wait times every 5 minutes while the locations are open. Using R, we scraped wait times for each service at each MVA office for 4 weeks from November 9, 2017 to December 6, 2017. Data scraped on holidays (Veteran’s Day: November 10-11, 2017 and Thanksgiving day: November 23, were excluded from prediction modeling.",
+            br(),
+            br(),
+            em("Prediction Model"),
+            br(),
+            br(),
+            "The prediction Model takes in user’s address, expected leaving date and time, and gives user the suggested MVA office to go where user can start his/her service first compared to other MVA offices. We use the wait times at every MVA offices from November 9, 2017 to December 6, 2017 as our training data to make predictions on expected wait time for future dates at different time slots. Our App takes account of actual travel time from user's location to MVA offices using Google Map API, predicts wait time at difference MVA offices for the planned service based on user's expected arrival time, and then reports the expected service start time at the suggested MVA office where minimizes both travel and wait time. Note that our App only reports the suggested MVA office to user when the user’s expected service start time is before the office closes.",
+            h3("Instructions"),
+            "1. Please type in your street address, city, and zip code (user location/departure address)",
+            br(),
+            "2. Please select your mode of transportation and service you would like to get at an MVA office.",
+            br(),
+            "3. Please type in the date of visit in the format of yyyy-mm-dd.",
+            br(),
+            "4. Please select the time of departure.",
+            br(),
+            "5. Click “Update” and wait for a few seconds to process your request.",
+            h3("Notes"),
+            "According to the Maryland MVA website (http://www.mva.maryland.gov/sebin/customerwaittimes/), the MVA's busiest days are at the end of each month and the first days of the next month, and MVA strongly recommends that you plan your visit during the middle of the month. In addition, MVA expect high volume days on Mondays, Fridays, and Saturdays overall, so plan your visit on a Tuesday, Wednesday, or Thursday to minimize your time at MVA.",
+            h3("Contributors to this App"),
+            "Su Jin Lim, Siruo Wang, Yeya Zheng, Jing Li, Chih-Kai Chang and Feiyang Zheng"),
+    tabItem(tabName = "recommendation", 
+            fluidRow(column(width = 8, 
+                            box(width = NULL, solidHeader = TRUE, h3("Our Recommendation"), 
+                                textOutput("office"),  leafletOutput("id.distPlot1", height = 550)), 
+                            box(width = NULL, status = "warning", h3("Estimated Travel Time and Distance From Your Location to MVA Offices"),
+                                DT::dataTableOutput("gmapresultTable"))), 
+                     column(width = 3, box(width = NULL, status = "warning", title="User Information", 
+                                           textInput(inputId="id.street", label="Street", value = "1 W Pratt St"),
+                                           textInput(inputId="id.city", label="City", value = "Baltimore"),
+                                           textInput(inputId="id.zipcode", label="Zipcode", value = "21201"),
+                                           selectInput(inputId = "id.travel.method",
+                                                       label = "Select Mode of Travel",
+                                                       choices = c("Driving","Walking","Bicycling","Transit")),
+                                           selectInput(inputId = "id.visit.reason",
+                                                       label = "Select Service",
+                                                       choices = c("Driver License Renewal", "Insurance Compliance Division", "Learners Permit",
+                                                                   "Other Drivers Services", "Other Vehicle Services", "Registration Renewal", 
+                                                                   "Tag Return","Title")),
+                                           textInput(inputId = "id.date", label="Date of Visit", value = "2018-01-01"),
+                                           
+                                           
+                                           uiOutput('slider1'),
+                                           # sliderInput(inputId = "id.time",
+                                           #             label = "Select Time",
+                                           #             min = as.POSIXct("08:30:00", format = "%H:%M:%S"),
+                                           #             max = as.POSIXct("16:30:00", format = "%H:%M:%S"),
+                                           #             value = as.POSIXct("09:00:00", format = "%H:%M:%S"),
+                                           #             step = 300,
+                                           #             timeFormat = "%H:%M:%S", ticks = F, animate = T),
+                                           submitButton("Update")
+                                           )
+                            
+                            )
+                     )), 
+    tabItem(tabName = "prediction",  
+            h2("Wait Time Trend at the Recommended Office"),plotOutput(outputId = "id.distPlot2"))
   )
 )
     
     
 
 dashboardPage(
+  skin = "yellow",
   header,
-  dashboardSidebar(disable = TRUE),
+  sidebar,
   body
 )
